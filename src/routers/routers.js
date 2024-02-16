96d51f674cb796ebc411a0b173026d260a405e23
LuanSilva <luanlvs2010@gmail.com>
2024-02-16 20:06:03 -0300

    feat: creating validation for email and password

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -1,6 +1,7 @@
 // No seu arquivo routes.mjs
 import  express  from "express"
 import UserController from "../controller/userController.js"
+import validator from "validator"
 
 class Router {
 
@@ -12,6 +13,8 @@ class Router {
     }
 
  async LoadRouter(){
+
+    let self = this
     
         this.router.get("/", async (req, res)=>{
 
@@ -37,13 +40,29 @@ class Router {
 
         this.router.post( "/api/users", async (req, res) =>{
 
+            let params = req.body
+
             try {
+
+                const emailIsValid = validator.isEmail(params.email)
+                const passwordIsValid = params.password.length
+
+                if (!emailIsValid){
+                    
+                    return res.status(400).json({err:"Invalid e-mail. Please provid a valid one."})
+                }
+
+                if(passwordIsValid < 6){
+
+                    return res.status(400).json({err: "Password too short! It must contain at least 6 characters."})
+                 
+                }
 
                 let user_controller = new UserController()
 
                 let result = await user_controller.CreateUser(req)
                 
-               res.json(result)
+                return res.status(201).json(result)
 
             } catch (error) {
 
